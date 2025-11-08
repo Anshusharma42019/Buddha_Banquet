7a74960cdbfe54d41a9eec39a8b429fd09f15431
Kritika Singh <kritikasingh273013@gmail.com>
2025-11-08 15:07:37 +0530

    done

--- src/pages/Students/UpdateBooking.jsx
+++ src/pages/Students/UpdateBooking.jsx
@@ -820,10 +820,9 @@ const UpdateBooking = () => {
                     onChange={handleInputChange}
                     value={booking.hall}
                   >
-                    <option value="Kitty Hall">Kitty Hall</option>
+                    <option value="Lawn">Lawn</option>
                     <option value="Banquet Hall">Banquet Hall</option>
-                    <option value="Rooftop Hall">Rooftop Hall</option>
-                    <option value="Flamingo Rooftop">Flamingo Rooftop </option>
+                    <option value="Lawn + Banquet Hall">Lawn + Banquet Hall</option>
                   </select>
                 </div>
               </div>
@@ -984,10 +983,9 @@ const UpdateBooking = () => {
                     onChange={handleInputChange}
                     value={booking.hall}
                   >
-                    <option value="Kitty Hall">Kitty Hall</option>
+                    <option value="Lawn">Lawn</option>
                     <option value="Banquet Hall">Banquet Hall</option>
-                    <option value="Rooftop Hall">Rooftop Hall</option>
-                    <option value="Flamingo Rooftop">Flamingo Rooftop </option>
+                    <option value="Lawn + Banquet Hall">Lawn + Banquet Hall</option>
                   </select>
                 </div>
               </div>
